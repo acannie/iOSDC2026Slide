5e493a92acc2ca52c83280ac3a333e1fcafc0b64
acannie <acannie0530@gmail.com>
2026-08-15 19:25:40 +0900

    目のアニメーションを実装

--- iOSDC2026Slide/05_SfSymbolsBasics/03_Animating.swift
+++ iOSDC2026Slide/05_SfSymbolsBasics/03_Animating.swift
@@ -49,71 +49,127 @@ struct Animating: View {
         }
     }
 
-    func iris(_ irisType: EyeType.IrisType, irisPosition: EyeType.IrisPosition) -> some View {
-        let originalIrisOffset = CGPoint(x: 325, y: -250)
-        let irisOffset = irisPosition.irisOffset
-        return ZStack {
-            Image(systemName: "circle.fill")
-                .resizable()
-                .foregroundStyle(
-                    LinearGradient(
-                        gradient: Gradient(colors: [.irisMiddle, .irisLight]),
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                )
-                .frame(width: 250, height: 275)
-            Image(systemName: "person.crop.circle.fill")
-                .resizable()
-                .foregroundStyle(
-                    LinearGradient(
-                        gradient: Gradient(colors: [.irisMiddle, .irisLight]),
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                )
-                .symbolRenderingMode(.multicolor)
-                .frame(width: 250, height: 275)
-        }
-        .offset(
-            x: originalIrisOffset.x + irisOffset.x,
-            y: originalIrisOffset.y + irisOffset.y
-        )
-    }
-
     var eye: some View {
-        var eyeAngle: Double {
-            switch count % 3 {
-            case 0: 0
-            case 1: 15
-            case 2: -15
-            default: 0
+        enum IrisPosition: Int, CaseIterable {
+            case center
+            case top
+            case bottom
+            case left
+            case right
+
+            var irisOffset: CGPoint {
+                switch self {
+                case .center:
+                    .zero
+                case .left:
+                    .init(x: -35, y: .zero)
+                case .right:
+                    .init(x: 35, y: .zero)
+                case .top:
+                    .init(x: .zero, y: -35)
+                case .bottom:
+                    .init(x: .zero, y: 35)
+                }
             }
         }
-        func eyebrowAngle(_ angle: Angle) -> Angle {
-            let eyebrowType = EyebrowType.neutral
-            return switch eyebrowType {
-            case .neutral, .surprise:
-                angle
-            case .anger:
-                angle + Angle(degrees: 15.0)
-            case .sadness:
-                angle + Angle(degrees: -10.0)
+        enum Expression: Int, CaseIterable {
+            case neutral
+            case anger
+            case sadness
+            case smile
+            case surprise
+            case dissatisfaction
+            case sleepy
+
+            var upperEyelashesOffsetY: CGFloat {
+                switch self {
+                case .neutral, .anger:
+                    0.0
+                case .sadness, .smile:
+                    25.0
+                case .dissatisfaction, .sleepy:
+                    50.0
+                case .surprise:
+                    -25.0
+                }
+            }
+
+            var lowerEyelashesOffsetY: CGFloat {
+                switch self {
+                case .neutral, .anger, .sadness, .smile, .dissatisfaction, .sleepy:
+                    0.0
+                case .surprise:
+                    25.0
+                }
+            }
+
+            var eyeAngle: Angle {
+                switch self {
+                case .neutral, .surprise, .dissatisfaction, .sleepy:
+                        .zero
+                case .anger:
+                    Angle(degrees: -10.0)
+                case .sadness, .smile:
+                    Angle(degrees: 5.0)
+                }
+            }
+
+            var eyeHeight: CGFloat {
+                switch self {
+                case .neutral:
+                    0.0
+                case .sadness, .smile:
+                    -50.0
+                case .dissatisfaction, .sleepy:
+                    -25.0
+                case .anger:
+                    25.0
+                case .surprise:
+                    50.0
+                }
+            }
+
+            func eyebrowAngle(_ angle: Angle) -> Angle {
+                switch self {
+                case .neutral, .surprise, .smile:
+                    angle
+                case .anger:
+                    angle + Angle(degrees: 15.0)
+                case .sadness, .sleepy, .dissatisfaction:
+                    angle + Angle(degrees: -10.0)
+                }
+            }
+
+            func eyebrowOffsetY(_ offsetY: CGFloat) -> CGFloat {
+                switch self {
+                case .neutral, .anger, .sadness, .smile, .dissatisfaction, .sleepy:
+                    offsetY
+                case .surprise:
+                    offsetY - 50.0
+                }
+            }
+
+            var irisPosition: IrisPosition {
+                switch self {
+                case .neutral:
+                    .center
+                case .sadness, .smile:
+                    .center
+                case .dissatisfaction, .sleepy:
+                    .center
+                case .anger:
+                    .center
+                case .surprise:
+                    .center
+                }
             }
         }
 
-        func eyebrowOffsetY(_ offsetY: CGFloat) -> CGFloat {
-            let eyebrowType = EyebrowType.neutral
-            return switch eyebrowType {
-            case .neutral, .anger, .sadness:
-                offsetY
-            case .surprise:
-                offsetY - 50.0
-            }
+        var expression: Expression {
+            let rawValue = (count / 3) % Expression.allCases.count
+            return Expression(rawValue: rawValue) ?? .neutral
         }
-        let expressionType = EyeType.ExpressionType.neutral
-        let irisType = EyeType.IrisType.normal
-        let irisPosition = EyeType.IrisPosition.center
+        let originalIrisOffset = CGPoint(x: 325, y: -250)
         return VStack(spacing: 32) {
             ZStack {
                 // 白目
@@ -121,61 +177,88 @@ struct Animating: View {
                     .resizable()
                     .foregroundStyle(.whiteOfEyes)
                     .rotationEffect(
-                        Angle(degrees: 20.0) + expressionType.eyeAngle
+                        Angle(degrees: 20.0) + expression.eyeAngle
                     )
-                    .frame(width: 350, height: 300.0 + expressionType.eyeHeight)
+                    .frame(width: 350, height: 300.0 + expression.eyeHeight)
                     .offset(x: 350, y: -250)
                 // 黒目
-                iris(irisType, irisPosition: irisPosition)
+                ZStack {
+                    Image(systemName: "circle.fill")
+                        .resizable()
+                        .foregroundStyle(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.irisMiddle, .irisLight]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .frame(width: 250, height: 275)
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .foregroundStyle(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.irisMiddle, .irisLight]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .symbolRenderingMode(.multicolor)
+                        .frame(width: 250, height: 275)
+                }
+                .offset(
+                    x: originalIrisOffset.x + expression.irisPosition.irisOffset.x,
+                    y: originalIrisOffset.y + expression.irisPosition.irisOffset.y
+                )
                 // アイライン
                 Image(systemName: "phone.down.fill")
                     .resizable()
                     .foregroundStyle(.eyelash)
                     .rotationEffect(
-                        Angle(degrees: 7.0) + expressionType.eyeAngle
+                        Angle(degrees: 7.0) + expression.eyeAngle
                     )
                     .frame(width: 475, height: 90)
-                    .offset(x: 400, y: -375 + expressionType.upperEyelashesOffsetY)
+                    .offset(x: 400, y: -375 + expression.upperEyelashesOffsetY)
                 // 二重線
                 Image(systemName: "button.angledtop.vertical.right.fill")
                     .resizable()
                     .foregroundStyle(.skinDark)
                     .rotationEffect(
-                        Angle(degrees: 275) + expressionType.eyeAngle
+                        Angle(degrees: 275) + expression.eyeAngle
                     )
                     .frame(width: 15, height: 350)
-                    .offset(x: 400, y: -425 + expressionType.upperEyelashesOffsetY)
+                    .offset(x: 400, y: -425 + expression.upperEyelashesOffsetY)
                 // 上まつ毛
                 Image(systemName: "line.3.horizontal.decrease")
                     .resizable()
                     .foregroundStyle(.eyelash)
                     .rotationEffect(
-                        Angle(degrees: 95) + expressionType.eyeAngle
+                        Angle(degrees: 95) + expression.eyeAngle
                     )
                     .frame(width: 75, height: 125)
-                    .offset(x: 450, y: -425 + expressionType.upperEyelashesOffsetY)
+                    .offset(x: 450, y: -425 + expression.upperEyelashesOffsetY)
                 // 下まつ毛
                 Image(systemName: "line.3.horizontal.decrease")
                     .resizable()
                     .foregroundStyle(.eyelash)
                     .rotationEffect(.degrees(80))
                     .frame(width: 25, height: 125)
-                    .offset(x: 425, y: -125 + expressionType.lowerEyelashesOffsetY)
+                    .offset(x: 425, y: -125 + expression.lowerEyelashesOffsetY)
                 // 眉毛
                 Image(systemName: "button.angledtop.vertical.left.fill")
                     .resizable()
                     .foregroundStyle(.eyebrow)
-                    .rotationEffect(eyebrowAngle(
+                    .rotationEffect(expression.eyebrowAngle(
                         Angle(degrees: 95)
                     ))
                     .frame(width: 25, height: 350)
                     .offset(
                         x: 350,
-                        y: eyebrowOffsetY(-550)
+                        y: expression.eyebrowOffsetY(-550)
                     )
             }
             .offset(x: -350, y: 300)
         }
+        .animation(.easeInOut, value: count)
         .background(
             Ellipse()
                 .fill(.skin)
@@ -183,111 +266,5 @@ struct Animating: View {
                 .offset(x: 20, y: -30)
                 .blur(radius: 16.0)
         )
-    }
-}
-
-enum EyebrowType: Hashable {
-    case neutral
-    case anger
-    case sadness
-    case surprise
-}
-
-enum EyeType: Hashable {
-    case normal(ExpressionType, IrisType, IrisPosition)
-    case laughter
-    case close
-    case closeTightly
-
-    enum ExpressionType {
-        case neutral
-        case anger
-        case sadness
-        case smile
-        case surprise
-        case dissatisfaction
-        case sleepy
-
-        var upperEyelashesOffsetY: CGFloat {
-            switch self {
-            case .neutral, .anger:
-                0.0
-            case .sadness, .smile:
-                25.0
-            case .dissatisfaction, .sleepy:
-                50.0
-            case .surprise:
-                -25.0
-            }
-        }
-
-        var lowerEyelashesOffsetY: CGFloat {
-            switch self {
-            case .neutral, .anger, .sadness, .smile, .dissatisfaction, .sleepy:
-                0.0
-            case .surprise:
-                25.0
-            }
-        }
-
-        var eyeAngle: Angle {
-            switch self {
-            case .neutral, .surprise, .dissatisfaction, .sleepy:
-                .zero
-            case .anger:
-                Angle(degrees: -10.0)
-            case .sadness, .smile:
-                Angle(degrees: 5.0)
-            }
-        }
-
-        var eyeHeight: CGFloat {
-            switch self {
-            case .neutral:
-                0.0
-            case .sadness, .smile:
-                -50.0
-            case .dissatisfaction, .sleepy:
-                -25.0
-            case .anger:
-                25.0
-            case .surprise:
-                50.0
-            }
-        }
-    }
-
-    enum IrisType {
-        case normal
-        case heart
-        case teary
-        case dizzy
-        case star
-        case knockout
-        case money
-        case lifeless
-    }
-
-    enum IrisPosition {
-        case center
-        case top
-        case bottom
-        case left
-        case right
-
-        var irisOffset: CGPoint {
-            switch self {
-            case .center:
-                .zero
-            case .left:
-                CGPoint(x: -35, y: .zero)
-            case .right:
-                CGPoint(x: 35, y: .zero)
-            case .top:
-                CGPoint(x: .zero, y: -35)
-            case .bottom:
-                CGPoint(x: .zero, y: 35)
-            }
-        }
     }
 }
