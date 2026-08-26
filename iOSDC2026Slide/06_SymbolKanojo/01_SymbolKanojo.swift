798b7412da6b3d0397a0408c04e2eae45015be88
acannie <acannie0530@gmail.com>
2026-08-26 13:58:12 +0900

    シンボルカノジョ。のスライドを準備中に差し替え

--- iOSDC2026Slide/06_SymbolKanojo/01_SymbolKanojo.swift
+++ iOSDC2026Slide/06_SymbolKanojo/01_SymbolKanojo.swift
@@ -10,8 +10,26 @@ import SwiftUI
 struct SymbolKanojo: View {
     var body: some View {
         VStack {
-            Text("SymbolKanojo")
+            HStack {
+                title
+                Spacer()
+            }
+            .padding(.leading, 64)
+            .padding(.top, 64)
+            Spacer()
+            Text("準備中")
+                .font(.system(size: 100, weight: .bold))
+                .foregroundStyle(.gray)
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+private extension SymbolKanojo {
+    var title: some View {
+        Text("コンテンツを作ろう")
+            .font(.system(size: 80, weight: .bold, design: .rounded))
+            .foregroundStyle(.introductionText)
     }
 }
--- iOSDC2026Slide/06_SymbolKanojo/SymbolKanojoScreen.swift
+++ iOSDC2026Slide/06_SymbolKanojo/SymbolKanojoScreen.swift
@@ -30,7 +30,6 @@ struct SymbolKanojoScreen: View {
                 goNextAction: { path.append(Destination.summary) }
             )
         }
-        .navigationTitle("シンボルカノジョ。")
         .navigationBarBackButtonHidden(true)
     }
 }
