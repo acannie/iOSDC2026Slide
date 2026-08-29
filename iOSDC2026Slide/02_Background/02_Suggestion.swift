c011baaa61aff84cb169a65c0736eead8ed3c607
acannie <acannie0530@gmail.com>
2026-08-29 16:24:31 +0900

    SF Symbols, SF Fonts,　Shapeの枠を作成

--- iOSDC2026Slide/02_Background/02_Suggestion.swift
+++ iOSDC2026Slide/02_Background/02_Suggestion.swift
@@ -8,41 +8,76 @@
 import SwiftUI
 
 struct Suggestion: View {
-    private enum CheckmarkItem: CaseIterable {
-        case sfSymbols
-        case sfFonts
-        case shape
-
-        var text: String {
-            switch self {
-            case .sfSymbols: "SF Symbols"
-            case .sfFonts: "SF Fonts"
-            case .shape: "Shape"
-            }
-        }
-    }
-
     var body: some View {
         VStack(alignment: .leading) {
-            ForEach(CheckmarkItem.allCases, id: \.self) { checkmarkItem in
-                item(checkmarkItem.text)
+            HStack {
+                title
+                Spacer()
             }
+            .padding(.leading, 64)
+            .padding(.top, 64)
+            HStack(spacing: 12) {
+                sfSymbols
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                divider
+                sfFonts
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                divider
+                shape
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .padding(.horizontal, 64)
+            .padding(.bottom, 64)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 private extension Suggestion {
-    func item(_ text: String) -> some View {
-        HStack(spacing: 32) {
-            Image(systemName: "checkmark")
-                .resizable()
-                .fontWeight(.semibold)
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(.green)
-                .frame(width: 100, height: 100)
-            Text(text)
-                .font(.system(size: 100, weight: .semibold))
+    var title: some View {
+        Text("Appleの標準リソース")
+            .font(.system(size: 80, weight: .bold, design: .rounded))
+            .foregroundStyle(.commonText)
+    }
+
+    func subtitle(_ text: String) -> some View {
+        Text(text)
+            .font(.system(size: 60, weight: .bold, design: .rounded))
+            .foregroundStyle(.commonText)
+    }
+
+    var sfSymbols: some View {
+        VStack {
+            subtitle("SF Symbols")
+            Rectangle()
+                .foregroundStyle(.red)
         }
+    }
+
+    var sfFonts: some View {
+        VStack {
+            subtitle("SF Fonts")
+            Rectangle()
+                .foregroundStyle(.green)
+        }
+    }
+
+    var shape: some View {
+        VStack {
+            HStack(alignment: .bottom, spacing: 12) {
+                Text("SwiftUI")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .foregroundStyle(.commonText)
+                subtitle("Shape")
+            }
+            Rectangle()
+                .foregroundStyle(.blue)
+        }
+    }
+
+    var divider: some View {
+        Capsule()
+            .fill(.guidelinesDivider)
+            .frame(width: 5, height: 750)
     }
 }
